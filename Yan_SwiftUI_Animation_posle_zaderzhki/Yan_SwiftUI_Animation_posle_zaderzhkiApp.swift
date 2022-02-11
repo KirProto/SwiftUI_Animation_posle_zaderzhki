cdb8a203cc442d26abb2d525778bed29a57cbe48
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 12:15:45 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation_posle_zaderzhki/Yan_SwiftUI_Animation_posle_zaderzhkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animation_posle_zaderzhkiApp.swift
+//  Yan_SwiftUI_Animation_posle_zaderzhki
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animation_posle_zaderzhkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
